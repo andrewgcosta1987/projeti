0fe30505c4325d47469250179b7dc176d6a510a9
andrewgcosta1987 <62894428+andrewgcosta1987@users.noreply.github.com>
2021-05-03 23:57:54 -0300

    Add files via upload

---
+++ view/menu.php
@@ -0,0 +1,43 @@
+<header class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light row">
+        	<img src="./control/img/logo05.jpg" alt="" class="navbar-brand col-sm-2 img-responsive">
+            <div id="navbarSite" class="collapse navbar-collapse col-sm-6">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a href="#" class="nav-link">Início</a>
+                	</li>
+                	<li class="nav-item">
+						<a href="#" class="nav-link">Bares e Restaurantes</a>
+                	</li>
+                	<li class="nav-item">
+						<a href="#" class="nav-link">Cardápio</a>
+                	</li>
+                	<li class="nav-item">
+						<a href="#" class="nav-link">Sobre</a>
+                	</li>
+                	<li class="nav-item">
+						<a href="#" class="nav-link">Orçamento</a>
+                	</li>
+                </ul>
+				<ul class="navbar-nav ml-2">
+					<li class="nav-item dropdown">
+						<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" id="navDrop">Social</a>
+						<div class="dropdown-menu">
+							<a href="" class="dropdown-item">Facebook</a>
+							<a href="" class="dropdown-item">Twiter</a>
+							<a href="" class="dropdown-item">instagram</a>
+						</div>
+					</li>
+				</ul>
+            </div>
+			<div class="col-sm-4 collapse navbar-collapse ml-2">
+				<form action="" class="form-inline ml-4">
+					<input type="search" class="form-control" placeholder="Buscar...">
+					<button class="btn btn-dark ml-2" type="submit">ok</button>
+				</form>
+			</div>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSite">
+				<spam class="navbar-toggler-icon"></spam>
+			</button>
+        </nav>
+    </header>
---
+++ view/topo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Primeiro layout do Projeti">
+    <title>Layout 1</title>
+    <link rel="stylesheet" href="./control/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" href="./control/css/estilo.css">
+    <script type="text/javascript" src="./control/js/jquery.js"></script>
+    <script type="text/javascript" src="./control/bootstrap/js/bootstrap.js"></script>
+</head>
+<body>
